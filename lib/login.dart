95e556f028213ce114f4c901e57c61b713244ddc
Deepak <deepakmkoshy@gmail.com>
2021-02-03 17:46:40 +0530

    Trying put new things

--- lib/login.dart
+++ lib/login.dart
@@ -168,47 +168,119 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
-        height: MediaQuery.of(context).size.height,
+        height: height,
         decoration: BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('assets/bg.jpg'), fit: BoxFit.cover)),
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SvgPicture.asset(
-              'assets/Confirmed-bro.svg',
-              fit: BoxFit.contain,
+            // SafeArea(
+            //   child: SvgPicture.asset(
+            //     'assets/Confirmed-bro.svg',
+            //     fit: BoxFit.contain,
+            //     height: height * 0.4,
+            //   ),
+            // ),
+            ClipPath(
+              clipper: TopCustomClipper(),
+              child: Container(
+                color: Colors.white,
+                height: height * 0.1,
+                width: width * 0.4,
+              ),
             ),
             Center(
               child: Container(
                 decoration: BoxDecoration(boxShadow: [
                   BoxShadow(
-                      blurRadius: 14,
-                      spreadRadius: 10,
-                      color: Colors.black.withOpacity(0.1))
+                    blurRadius: 14,
+                    spreadRadius: 5,
+                    color: Colors.black.withOpacity(0.1),
+                  ),
                 ]),
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(16.0),
                   child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 8.0, sigmaY: 8.0),
+                    filter: ImageFilter.blur(sigmaX: 16.0, sigmaY: 16.0),
                     child: Container(
-                      height: 100,
-                      width: 300,
+                      height: height * 0.5,
+                      width: width * 0.8,
                       decoration: BoxDecoration(
                         color: Colors.white.withOpacity(0.2),
                         borderRadius: BorderRadius.circular(16.0),
                         border: Border.all(
                             width: 1.5, color: Colors.white.withOpacity(0.2)),
                       ),
-                      child: Center(child: Text('Glass Morphism'),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Login With',
+                              style: TextStyle(
+                                  fontSize: width / 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            RaisedButton(
+                              color: Colors.white.withOpacity(0.8),
+                              elevation: 10,
+                              onPressed: () {
+                                signInWithGoogle().then((result) {
+                                  if (result != null) {
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) {
+                                          return FirstScreen();
+                                        },
+                                      ),
+                                    );
+                                  }
+                                });
+                              },
+                              splashColor: Colors.grey,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                side: BorderSide(
+                                  color: Colors.white.withOpacity(0.3),
+                                ),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Image(
+                                        image: AssetImage(
+                                            "assets/google_logo.png"),
+                                        height: 35.0),
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 10),
+                                      child: Text(
+                                        'Sign in with Google',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
             ),
             // SizedBox(height: 50),
             // googleLoginButton(),
@@ -260,3 +332,19 @@ class _LoginState extends State<Login> {
 //     );
 //   }
 // }
+
+class TopCustomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.moveTo(0, 0);
+    path.quadraticBezierTo(
+        size.width / 2, size.height / 2, size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
